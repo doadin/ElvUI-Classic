d3acf29aaf63f4f2f46afde6bb436f134356bd3b
Azilroka <darktemplar9900@yahoo.com>
2019-08-10 22:53:05 -0500

    /KB & Groups

--- ElvUI/init.lua
+++ ElvUI/init.lua
@@ -442,31 +442,3 @@ if (_G.UIDROPDOWNMENU_OPEN_PATCH_VERSION or 0) < 1 then
 		end
 	end)
 end
-
---CommunitiesUI taint workaround #2
---credit: https://www.townlong-yak.com/bugs/YhgQma-SetValueRefreshTaint
-if (_G.COMMUNITY_UIDD_REFRESH_PATCH_VERSION or 0) < 1 then
-	_G.COMMUNITY_UIDD_REFRESH_PATCH_VERSION = 1
-	local function CleanDropdowns()
-		if _G.COMMUNITY_UIDD_REFRESH_PATCH_VERSION ~= 1 then
-			return
-		end
-		local f, f2 = _G.FriendsFrame, _G.FriendsTabHeader
-		local s = f:IsShown()
-		f:Hide()
-		f:Show()
-		if not f2:IsShown() then
-			f2:Show()
-			f2:Hide()
-		end
-		if not s then
-			f:Hide()
-		end
-	end
-	hooksecurefunc("Communities_LoadUI", CleanDropdowns)
-	hooksecurefunc("SetCVar", function(n)
-		if n == "lastSelectedClubId" then
-			CleanDropdowns()
-		end
-	end)
-end
