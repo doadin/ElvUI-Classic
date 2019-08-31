8f4dd92e3b0409a594b1e6c7fd88a77f8dedc86a
Azilroka <darktemplar9900@yahoo.com>
2019-08-31 13:40:46 -0500

    Pass info through from oUF instead of calling UnitAura again :D

--- ElvUI/Modules/Nameplates/Elements/Auras.lua
+++ ElvUI/Modules/Nameplates/Elements/Auras.lua
@@ -32,24 +32,21 @@ function NP:Buffs_PostCreateIcon(button)
 	NP:Construct_AuraIcon(button)
 end
 
-function NP:Buffs_PostUpdateIcon(unit, button, index)
-	local Name, _, _, _, Duration, ExpirationTime, UnitCaster, _, _, SpellID = UnitAura(unit, index, button.filter)
-
-	if Duration == 0 and ExpirationTime == 0 then
-		Duration, ExpirationTime = E.Libs.LCD:GetAuraDurationByUnit(unit, SpellID, UnitCaster, Name)
+function NP:Buffs_PostUpdateIcon(unit, button, index, position, duration, expiration, debuffType, isStealable, unitCaster, spellID, name)
+	if duration == 0 and expiration == 0 then
+		duration, expiration = E.Libs.LCD:GetAuraDurationByUnit(unit, spellID, unitCaster, name)
 
 		button.IsLibClassicDuration = true
 	end
 
 	if (button.cd) and (button.IsLibClassicDuration) then
-		if (Duration and Duration > 0) then
-			button.cd:SetCooldown(ExpirationTime - Duration, Duration)
+		if (duration and duration > 0) then
+			button.cd:SetCooldown(expiration - duration, duration)
 			button.cd:Show()
 		else
 			button.cd:Hide()
 		end
 	end
-
 	NP:PostUpdateAura(unit, button)
 end
 
@@ -62,18 +59,16 @@ function NP:Debuffs_PostCreateIcon(button)
 	NP:Construct_AuraIcon(button)
 end
 
-function NP:Debuffs_PostUpdateIcon(unit, button, index, position, duration, expiration, debuffType, isStealable)
-	local Name, _, _, _, Duration, ExpirationTime, UnitCaster, _, _, SpellID = UnitAura(unit, index, button.filter)
-
-	if Duration == 0 and ExpirationTime == 0 then
-		Duration, ExpirationTime = E.Libs.LCD:GetAuraDurationByUnit(unit, SpellID, UnitCaster, Name)
+function NP:Debuffs_PostUpdateIcon(unit, button, index, position, duration, expiration, debuffType, isStealable, unitCaster, spellID, name)
+	if duration == 0 and expiration == 0 then
+		duration, expiration = E.Libs.LCD:GetAuraDurationByUnit(unit, spellID, unitCaster, name)
 
 		button.IsLibClassicDuration = true
 	end
 
 	if (button.cd) and (button.IsLibClassicDuration) then
-		if (Duration and Duration > 0) then
-			button.cd:SetCooldown(ExpirationTime - Duration, Duration)
+		if (duration and duration > 0) then
+			button.cd:SetCooldown(expiration - duration, duration)
 			button.cd:Show()
 		else
 			button.cd:Hide()
