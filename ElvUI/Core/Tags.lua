07e562de9b7654da0031b04d7dc2c991b24f6a66
Azilroka <darktemplar9900@yahoo.com>
2019-09-03 21:29:22 -0500

    Move Tags

--- ElvUI/Core/Tags.lua
+++ ElvUI/Core/Tags.lua
@@ -1,7 +1,7 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local _, ns = ...
-local ElvUF = ns.oUF
-assert(ElvUF, "ElvUI was unable to locate oUF.")
+local ElvUF = ns.ElvUF
+assert(ElvUF, "ElvUI was unable to locate ElvUF.")
 local Translit = E.Libs.Translit
 local translitMark = "!"
 
@@ -43,19 +43,19 @@ local UnitPower = UnitPower
 local UnitPowerMax = UnitPowerMax
 local UnitPowerType = UnitPowerType
 local UnitReaction = UnitReaction
-local UnitStagger = UnitStagger
 
-local ALTERNATE_POWER_INDEX = ALTERNATE_POWER_INDEX
 local DEFAULT_AFK_MESSAGE = DEFAULT_AFK_MESSAGE
 local PVP = PVP
-local SPEC_MONK_BREWMASTER = SPEC_MONK_BREWMASTER
-local SPEC_PALADIN_RETRIBUTION = SPEC_PALADIN_RETRIBUTION
-local SPELL_POWER_CHI = Enum.PowerType.Chi
-local SPELL_POWER_HOLY_POWER = Enum.PowerType.HolyPower
-local SPELL_POWER_MANA = Enum.PowerType.Mana
-local SPELL_POWER_SOUL_SHARDS = Enum.PowerType.SoulShards
 local UNITNAME_SUMMON_TITLE17 = UNITNAME_SUMMON_TITLE17
 local UNKNOWN = UNKNOWN
+
+local GetCVarBool = GetCVarBool
+local GetNumQuestLogEntries = GetNumQuestLogEntries
+local GetQuestDifficultyColor = GetQuestDifficultyColor
+local GetQuestLogTitle = GetQuestLogTitle
+local UnitPVPName = UnitPVPName
+local LEVEL = LEVEL
+
 -- GLOBALS: Hex, PowerBarColor, _TAGS
 
 ------------------------------------------------------------------------
@@ -998,4 +998,122 @@ ElvUF.Tags.Events['target:translit'] = 'UNIT_TARGET'
 ElvUF.Tags.Methods['target:translit'] = function(unit)
 	local targetName = Translit:Transliterate(UnitName(unit.."target"), translitMark)
 	return targetName or nil
+end
+
+ElvUF.Tags.Events['npctitle'] = 'UNIT_NAME_UPDATE'
+ElvUF.Tags.Methods['npctitle'] = function(unit)
+	if (UnitIsPlayer(unit)) then
+		return
+	end
+
+	E.ScanTooltip:SetOwner(_G.UIParent, "ANCHOR_NONE")
+	E.ScanTooltip:SetUnit(unit)
+	E.ScanTooltip:Show()
+
+	local Title = _G[format('ElvUI_ScanTooltipTextLeft%d', GetCVarBool('colorblindmode') and 3 or 2)]:GetText()
+
+	if (Title and not Title:find('^'..LEVEL)) then
+		return Title
+	end
+end
+
+ElvUF.Tags.Events['guild:rank'] = 'UNIT_NAME_UPDATE'
+ElvUF.Tags.Methods['guild:rank'] = function(unit)
+	if (UnitIsPlayer(unit)) then
+		return select(2, GetGuildInfo(unit)) or ''
+	end
+end
+
+ElvUF.Tags.Events['class'] = 'UNIT_NAME_UPDATE'
+ElvUF.Tags.Methods['class'] = function(unit)
+	return UnitClass(unit)
+end
+
+ElvUF.Tags.Events['name:title'] = 'UNIT_NAME_UPDATE'
+ElvUF.Tags.Methods['name:title'] = function(unit)
+	if (UnitIsPlayer(unit)) then
+		return UnitPVPName(unit)
+	end
+end
+
+ElvUF.Tags.SharedEvents.QUEST_LOG_UPDATE = true
+
+ElvUF.Tags.Events['quest:title'] = 'QUEST_LOG_UPDATE'
+ElvUF.Tags.Methods['quest:title'] = function(unit)
+	if UnitIsPlayer(unit) then
+		return
+	end
+
+	E.ScanTooltip:SetOwner(_G.UIParent, "ANCHOR_NONE")
+	E.ScanTooltip:SetUnit(unit)
+	E.ScanTooltip:Show()
+
+	local QuestName
+
+	if E.ScanTooltip:NumLines() >= 3 then
+		for i = 3, E.ScanTooltip:NumLines() do
+			local QuestLine = _G['ElvUI_ScanTooltipTextLeft' .. i]
+			local QuestLineText = QuestLine and QuestLine:GetText()
+
+			local PlayerName, ProgressText = strmatch(QuestLineText, '^ ([^ ]-) ?%- (.+)$')
+
+			if not ( PlayerName and PlayerName ~= '' and PlayerName ~= UnitName('player') ) then
+				if ProgressText then
+					QuestName = _G['ElvUI_ScanTooltipTextLeft' .. i - 1]:GetText()
+				end
+			end
+		end
+		for i = 1, GetNumQuestLogEntries() do
+			local title, level, _, isHeader = GetQuestLogTitle(i)
+			if not isHeader and title == QuestName then
+				local colors = GetQuestDifficultyColor(level)
+				return Hex(colors.r, colors.g, colors.b)..QuestName..'|r'
+			end
+		end
+	end
+end
+
+ElvUF.Tags.Events['quest:info'] = 'QUEST_LOG_UPDATE'
+ElvUF.Tags.Methods['quest:info'] = function(unit)
+	if UnitIsPlayer(unit) then
+		return
+	end
+
+	E.ScanTooltip:SetOwner(_G.UIParent, "ANCHOR_NONE")
+	E.ScanTooltip:SetUnit(unit)
+	E.ScanTooltip:Show()
+
+	local ObjectiveCount = 0
+	local QuestName
+
+	if E.ScanTooltip:NumLines() >= 3 then
+		for i = 3, E.ScanTooltip:NumLines() do
+			local QuestLine = _G['ElvUI_ScanTooltipTextLeft' .. i]
+			local QuestLineText = QuestLine and QuestLine:GetText()
+
+			local PlayerName, ProgressText = strmatch(QuestLineText, '^ ([^ ]-) ?%- (.+)$')
+			if (not PlayerName or PlayerName == '' or PlayerName == UnitName('player')) and ProgressText then
+				local x, y
+				if not QuestName and ProgressText then
+					QuestName = _G['ElvUI_ScanTooltipTextLeft' .. i - 1]:GetText()
+				end
+				if ProgressText then
+					x, y = strmatch(ProgressText, '(%d+)/(%d+)')
+					if x and y then
+						local NumLeft = y - x
+						if NumLeft > ObjectiveCount then -- track highest number of objectives
+							ObjectiveCount = NumLeft
+							if ProgressText then
+								return ProgressText
+							end
+						end
+					else
+						if ProgressText then
+							return QuestName .. ': ' .. ProgressText
+						end
+					end
+				end
+			end
+		end
+	end
 end
