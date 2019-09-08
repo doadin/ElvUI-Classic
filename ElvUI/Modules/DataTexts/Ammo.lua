022c3d84966352925b4b18e410e07c8e2ec618ef
Azilroka <darktemplar9900@yahoo.com>
2019-09-08 04:33:20 -0500

    Ammo/Shard DataText

--- ElvUI/Modules/DataTexts/Ammo.lua
+++ ElvUI/Modules/DataTexts/Ammo.lua
@@ -1,4 +1,4 @@
-local E, L, V, P, G = unpack(ElvUI)
+local E, L, V, P, G = unpack(select(2, ...))
 local DT = E:GetModule("DataTexts")
 
 local _G = _G
@@ -7,12 +7,11 @@ local format, join = string.format, string.join
 
 local GetItemInfo = GetItemInfo
 local GetItemCount = GetItemCount
-local GetAuctionItemSubClasses = GetAuctionItemSubClasses
 local GetContainerItemID = GetContainerItemID
-local GetContainerItemLink = GetContainerItemLin
+local GetContainerItemLink = GetContainerItemLink
 local GetInventoryItemLink = GetInventoryItemLink
 local GetInventoryItemCount = GetInventoryItemCount
-local GetInventorySlotInfo = GetInventorySlotInfo
+local GetInventoryItemID = GetInventoryItemID
 local ContainerIDToInventoryID = ContainerIDToInventoryID
 local GetContainerNumSlots = GetContainerNumSlots
 local GetContainerNumFreeSlots = GetContainerNumFreeSlots
@@ -21,30 +20,20 @@ local NUM_BAG_SLOTS = NUM_BAG_SLOTS
 local NUM_BAG_FRAMES = NUM_BAG_FRAMES
 local INVTYPE_AMMO = INVTYPE_AMMO
 
-local quiver = select(1, GetAuctionItemSubClasses(8))
-local pouch = select(2, GetAuctionItemSubClasses(8))
-local soulBag = select(2, GetAuctionItemSubClasses(3))
-
 local iconString = "|T%s:16:16:0:0:64:64:4:55:4:55|t"
 local displayString = ""
 
 local lastPanel
 
 local function OnEvent(self)
-	local name, count, link
+	local name, count, itemID
 	if E.myclass == "WARLOCK" then
-		name = GetItemInfo(6265)
-		count = GetItemCount(6265)
-		if count > 0 then
-			self.text:SetFormattedText(displayString, name, count)
-		else
-			self.text:SetFormattedText(displayString, name, 0)
-		end
+		name, count = GetItemInfo(6265), GetItemCount(6265)
+		self.text:SetFormattedText(displayString, name or 'Soul Shard', count or 0) -- Does not need localized. It gets updated.
 	else
-		link = GetInventoryItemLink("player", GetInventorySlotInfo("AmmoSlot"))
-		count = GetInventoryItemCount("player", GetInventorySlotInfo("AmmoSlot"))
-		if link and (count > 0) then
-			name = GetItemInfo(link)
+		itemID, count = GetInventoryItemID("player", INVSLOT_AMMO), GetInventoryItemCount("player", INVSLOT_AMMO)
+		if itemID and (count > 0) then
+			name = GetItemInfo(itemID)
 			self.text:SetFormattedText(displayString, name, count)
 		else
 			self.text:SetFormattedText(displayString, INVTYPE_AMMO, 0)
@@ -56,43 +45,45 @@ end
 
 local function OnEnter(self)
 	DT:SetupTooltip(self)
-	DT.tooltip:AddLine(INVTYPE_AMMO)
 
 	local r, g, b
 	local item, link, count
-	local _, name, quality, subclass, equipLoc, texture
+	local _, name, quality, itemSubType, equipLoc, texture, itemClassID, itemSubClassID
 	local free, total, used
 
-	for i = 0, NUM_BAG_FRAMES do
-		for j = 1, GetContainerNumSlots(i) do
-			item = GetContainerItemID(i, j)
-			if item then
-				link = GetContainerItemLink(i, j)
-				name, _, quality, _, _, _, _, _, equipLoc, texture = GetItemInfo(link)
-				count = GetItemCount(link)
+	if E.myclass == 'HUNTER' then
+		DT.tooltip:AddLine(INVTYPE_AMMO)
 
-				if equipLoc == "INVTYPE_AMMO" then
-					r, g, b = GetItemQualityColor(quality)
-					DT.tooltip:AddDoubleLine(join("", format(iconString, texture), " ", name), count, r, g, b)
+		for i = 0, NUM_BAG_FRAMES do
+			for j = 1, GetContainerNumSlots(i) do
+				item = GetContainerItemID(i, j)
+				if item then
+					link = GetContainerItemLink(i, j)
+					name, _, quality, _, _, _, _, _, equipLoc, texture = GetItemInfo(link)
+					count = GetItemCount(link)
+
+					if equipLoc == "INVTYPE_AMMO" then
+						r, g, b = GetItemQualityColor(quality)
+						DT.tooltip:AddDoubleLine(join("", format(iconString, texture), " ", name), count, r, g, b)
+					end
 				end
 			end
 		end
-	end
 
-	DT.tooltip:AddLine(" ")
+		DT.tooltip:AddLine(" ")
+	end
 
 	for i = 1, NUM_BAG_SLOTS do
 		link = GetInventoryItemLink("player", ContainerIDToInventoryID(i))
 		if link then
-			name, _, quality, _, _, _, subclass, _, _, texture = GetItemInfo(link)
-
-			if subclass == quiver or subclass == pouch or subclass == soulBag then
+			name, _, quality, _, _, _, itemSubType, _, _, texture, itemClassID, itemSubClassID = GetItemInfo(link)
+			if itemSubClassID == LE_ITEM_CLASS_QUIVER or itemClassID == LE_ITEM_CLASS_CONTAINER and itemSubClassID == 1 then
 				r, g, b = GetItemQualityColor(quality)
 
 				free, total = GetContainerNumFreeSlots(i), GetContainerNumSlots(i)
 				used = total - free
 
-				DT.tooltip:AddLine(subclass)
+				DT.tooltip:AddLine(itemSubType)
 				DT.tooltip:AddDoubleLine(join("", format(iconString, texture), "  ", name), format("%d / %d", used, total), r, g, b)
 			end
 		end
@@ -103,18 +94,18 @@ end
 
 local function OnClick(_, btn)
 	if btn == "LeftButton" then
-		if not E.bags then
+		if not E.private.bags.enable then
 			for i = 1, NUM_BAG_SLOTS do
 				local link = GetInventoryItemLink("player", ContainerIDToInventoryID(i))
 				if link then
-					local subclass = select(7, GetItemInfo(link))
-					if subclass == quiver or subclass == pouch or subclass == soulBag then
+					local itemClassID, itemSubClassID = select(11, GetItemInfo(link))
+					if itemSubClassID == LE_ITEM_CLASS_QUIVER or itemClassID == LE_ITEM_CLASS_CONTAINER and itemSubClassID == 1 then
 						_G.ToggleBag(i)
 					end
 				end
 			end
 		else
-			_G.OpenAllBags()
+			ToggleAllBags()
 		end
 	end
 end
