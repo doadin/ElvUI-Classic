3e3ce919e5d12efc4917d021f6e3676d8f299119
Azilroka <darktemplar9900@yahoo.com>
2019-09-22 09:59:34 -0500

    Revert "Let Minimap Module do it's job on it's own."
    This reverts commit d988e0e75eb5bb23625b05b335e0ff8f530e4a49.

--- ElvUI/Layout/Layout.lua
+++ ElvUI/Layout/Layout.lua
@@ -452,8 +452,13 @@ function LO:CreateMinimapPanels()
 	rminipanel:SetTemplate(E.db.datatexts.panelTransparency and 'Transparent' or 'Default', true)
 	DT:RegisterPanel(rminipanel, 1, 'ANCHOR_BOTTOM', 0, -4)
 
-	_G.LeftMiniPanel:Hide()
-	_G.RightMiniPanel:Hide()
+	if E.db.datatexts.minimapPanels then
+		_G.LeftMiniPanel:Show()
+		_G.RightMiniPanel:Show()
+	else
+		_G.LeftMiniPanel:Hide()
+		_G.RightMiniPanel:Hide()
+	end
 
 	local f = CreateFrame("Frame", 'BottomMiniPanel', Minimap)
 	f:SetPoint("BOTTOM", Minimap, "BOTTOM")
