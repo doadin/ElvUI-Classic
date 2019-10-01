d1c538427c90614c25d43016969750ff00387f16
Azilroka <darktemplar9900@yahoo.com>
2019-10-01 15:48:02 -0500

    Crit / Spell Crit

--- ElvUI/Modules/DataTexts/SpellCrit.lua
+++ ElvUI/Modules/DataTexts/SpellCrit.lua
@@ -4,23 +4,13 @@ local DT = E:GetModule('DataTexts')
 --Lua functions
 local strjoin = strjoin
 --WoW API / Variables
-local GetCritChance = GetCritChance
-local GetRangedCritChance = GetRangedCritChance
 local GetSpellCritChance = GetSpellCritChance
 local CRIT_ABBR = CRIT_ABBR
 
 local displayString, lastPanel = ''
 
 local function OnEvent(self)
-	local critRating
-
-	if E.myclass == "HUNTER" then
-		critRating = GetRangedCritChance()
-	else
-		critRating = GetCritChance()
-	end
-
-	self.text:SetFormattedText(displayString, CRIT_ABBR, critRating)
+	self.text:SetFormattedText(displayString, CRIT_ABBR, GetSpellCritChance())
 
 	lastPanel = self
 end
@@ -34,4 +24,4 @@ local function ValueColorUpdate(hex)
 end
 E.valueColorUpdateFuncs[ValueColorUpdate] = true
 
-DT:RegisterDatatext('Crit Chance', {"UNIT_STATS", "UNIT_AURA", "PLAYER_DAMAGE_DONE_MODS"}, OnEvent, nil, nil, nil, nil, STAT_CRITICAL_STRIKE)
+DT:RegisterDatatext('Spell Crit Chance', {"UNIT_STATS", "UNIT_AURA", "PLAYER_DAMAGE_DONE_MODS"}, OnEvent, nil, nil, nil, nil, 'Spell Crit Chance')
