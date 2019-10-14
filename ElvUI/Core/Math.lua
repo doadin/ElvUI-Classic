87d0ca69414a6df03ac31ed053462de398d1b146
Merathilis <neo0608@gmail.com>
2019-10-14 19:03:09 +0200

    Protect E:GetTimeInfo from infinity math thing. @Azilroka we must blame CatSkull if anything is not working.

--- ElvUI/Core/Math.lua
+++ ElvUI/Core/Math.lua
@@ -324,9 +324,14 @@ E.TimeFormats = { -- short and long aura time formats
 local DAY, HOUR, MINUTE = 86400, 3600, 60 --used for calculating aura time text
 local DAYISH, HOURISH, MINUTEISH = HOUR * 23.5, MINUTE * 59.5, 59.5 --used for caclculating aura time at transition points
 local HALFDAYISH, HALFHOURISH, HALFMINUTEISH = DAY/2 + 0.5, HOUR/2 + 0.5, MINUTE/2 + 0.5 --used for calculating next update times
+local Infinity = math.huge
 
 -- will return the the value to display, the formatter id to use and calculates the next update for the Aura
 function E:GetTimeInfo(s, threshhold, hhmm, mmss)
+	if s == Infinity then
+		s = 0
+	end
+
 	if s < MINUTE then
 		if s >= threshhold then
 			return floor(s), 3, 0.51
