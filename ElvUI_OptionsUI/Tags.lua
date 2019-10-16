e6df577d9c5459982683d5daa046e6eda2567d9b
Azilroka <darktemplar9900@yahoo.com>
2019-10-16 15:07:20 -0500

    Alright @Luckyone

---
+++ ElvUI_OptionsUI/Tags.lua
@@ -0,0 +1,48 @@
+local E, _, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local C, L = unpack(select(2, ...))
+
+E.Options.args.tagGroup = {
+	order = 925,
+	type = "group",
+	name = L["Available Tags"],
+	args = {
+		header = {
+			order = 1,
+			type = "header",
+			name = L["Available Tags"],
+		},
+		general = {
+			order = 2,
+			type = "group",
+			name = "General",
+			guiInline = true,
+			childGroups = 'tab',
+			args = {},
+		},
+	}
+}
+
+E.TagInfo = {
+	['namecolor'] = { category = 'Name', description = "Color Names by Player Class / NPC Reaction" }
+}
+
+for Tag in next, E.oUF.Tags.Events do
+	if not E.TagInfo[Tag] then
+		E.TagInfo[Tag] = { category = 'Miscellanous', description = "" }
+	end
+
+	if not E.Options.args.tagGroup.args.general.args[E.TagInfo[Tag].category] then
+		E.Options.args.tagGroup.args.general.args[E.TagInfo[Tag].category] = {
+			order = 925,
+			type = "group",
+			name = E.TagInfo[Tag].category,
+			args = {}
+		}
+	end
+
+	E.Options.args.tagGroup.args.general.args[E.TagInfo[Tag].category].args[Tag] = {
+		type = "description",
+		fontSize = "medium",
+		name = format('%s - %s', Tag, E.TagInfo[Tag] and E.TagInfo[Tag].description or ''),
+	}
+end
