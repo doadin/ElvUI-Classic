26e5c84f8f1553bdce79bafad9278c785b1da7ed
Azilroka <darktemplar9900@yahoo.com>
2019-10-20 15:33:28 -0500

    AuraFilter - check to see if the header has a db if not attached to button/statusbar

--- ElvUI/Modules/UnitFrames/Elements/Auras.lua
+++ ElvUI/Modules/UnitFrames/Elements/Auras.lua
@@ -431,7 +431,7 @@ end
 function UF:AuraFilter(unit, button, name, _, _, debuffType, duration, expiration, caster, isStealable, _, spellID, _, isBossDebuff, casterIsPlayer)
 	if not name then return end -- checking for an aura that is not there, pass nil to break while loop
 
-	local db = button.db
+	local db = button.db or self.db
 	if not db then return true elseif db.filters then db = db.filters end
 
 	local isPlayer = (caster == 'player' or caster == 'vehicle')
