1b2c54b00fff388eeed6b609b260c8f542fa11d0
Azilroka <darktemplar9900@yahoo.com>
2019-10-20 14:33:08 -0500

    AuraBars - Fix DB Attachment

--- ElvUI/Libraries/oUF_Plugins/oUF_AuraBars/oUF_AuraBars.lua
+++ ElvUI/Libraries/oUF_Plugins/oUF_AuraBars/oUF_AuraBars.lua
@@ -270,7 +270,7 @@ local function UpdateAuras(self, event, unit)
 		local fromRange, toRange
 
 		if(element.PreSetPosition) then
-			fromRange, toRange = element:PreSetPosition(visible)
+			fromRange, toRange = element:PreSetPosition(element.maxBars)
 		end
 
 		if(fromRange or element.createdBars > element.anchoredBars) then
