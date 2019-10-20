98a1e5390cb7f06233f62a3bf316afe43e875642
Azilroka <darktemplar9900@yahoo.com>
2019-10-20 14:08:54 -0500

    AuraWatch Error Fix

--- ElvUI/Libraries/oUF_Plugins/oUF_AuraWatch/oUF_AuraWatch.lua
+++ ElvUI/Libraries/oUF_Plugins/oUF_AuraWatch/oUF_AuraWatch.lua
@@ -145,7 +145,7 @@ local function updateIcon(element, unit, index, offset, filter, isDebuff, visibl
 			if(button.icon) then button.icon:SetTexture(texture) end
 			if(button.count) then button.count:SetText(count > 1 and count) end
 
-			local size = setting.sizeOverride and setting.sizeOverride > 0 and setting.sizeOverride or element.size or 16
+			local size = setting.sizeOverride and setting.sizeOverride > 0 and setting.sizeOverride or element.size
 			button:SetSize(size, size)
 
 			button:SetID(index)
@@ -188,7 +188,8 @@ local function onlyShowMissingIcon(element, unit, _, offset)
 		if(button.icon) then button.icon:SetTexture(GetSpellTexture(SpellID)) end
 		if(button.overlay) then button.overlay:Hide() end
 
-		local size = setting.sizeOverride > 0 and setting.sizeOverride or element.size
+		local size = setting.sizeOverride and setting.sizeOverride > 0 and setting.sizeOverride or element.size
+
 		button:SetSize(size, size)
 		button.spellID = SpellID
 
