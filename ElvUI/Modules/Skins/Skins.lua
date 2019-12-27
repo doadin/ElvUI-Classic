695c2eb28c4f749076034532b2baf034f3590d5b
Simpy <kodi03466@hotmail.com>
2019-12-27 15:46:05 -0500

    this needs to execute on init because skins have hooks which hook onto functions (like blizzard functions), if we execute this on PEW they will hook but the function will have already executed and the hook will miss
    # Conflicts:
    #	ElvUI/Modules/Skins/Skins.lua

--- ElvUI/Modules/Skins/Skins.lua
+++ ElvUI/Modules/Skins/Skins.lua
@@ -1376,22 +1376,6 @@ do
 	end
 end
 
-function S:PLAYER_ENTERING_WORLD()
-	for addonName, object in pairs(self.addonsToLoad) do
-		local isLoaded, isFinished = IsAddOnLoaded(addonName)
-		if isLoaded and isFinished then
-			S:CallLoadedAddon(addonName, object)
-		end
-	end
-
-	for index, loadFunc in ipairs(self.nonAddonsToLoad) do
-		xpcall(loadFunc, errorhandler)
-		self.nonAddonsToLoad[index] = nil
-	end
-
-	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-end
-
 function S:RegisterSkin(addonName, loadFunc, forceLoad, bypass)
 	if bypass then
 		self.allowBypass[addonName] = true
@@ -1432,7 +1416,17 @@ function S:Initialize()
 	self.Initialized = true
 	self.db = E.private.skins
 
-	S:RegisterEvent('PLAYER_ENTERING_WORLD')
+	for index, loadFunc in ipairs(self.nonAddonsToLoad) do
+		xpcall(loadFunc, errorhandler)
+		self.nonAddonsToLoad[index] = nil
+	end
+
+	for addonName, object in pairs(self.addonsToLoad) do
+		local isLoaded, isFinished = IsAddOnLoaded(addonName)
+		if isLoaded and isFinished then
+			S:CallLoadedAddon(addonName, object)
+		end
+	end
 
 	hooksecurefunc("TriStateCheckbox_SetState", function(_, checkButton)
 		if checkButton.forceSaturation then
