e65c41eacf2d20a04d46dccad1e4263b50b461c9
Merathilis <neo0608@gmail.com>
2020-01-04 20:14:42 +0100

    Fix whoopsi from last commit.

--- ElvUI/Modules/UnitFrames/Elements/RaidRoleIcons.lua
+++ ElvUI/Modules/UnitFrames/Elements/RaidRoleIcons.lua
@@ -8,13 +8,16 @@ function UF:Construct_RaidRoleFrames(frame)
 	local anchor = CreateFrame('Frame', nil, frame.RaisedElementParent)
 	frame.LeaderIndicator = anchor:CreateTexture(nil, 'OVERLAY')
 	frame.AssistantIndicator = anchor:CreateTexture(nil, 'OVERLAY')
+	frame.MasterLooterIndicator = anchor:CreateTexture(nil, 'OVERLAY')
 
 	anchor:Size(24, 12)
 	frame.LeaderIndicator:Size(12)
 	frame.AssistantIndicator:Size(12)
+	frame.MasterLooterIndicator:Size(12)
 
 	frame.LeaderIndicator.PostUpdate = UF.RaidRoleUpdate
 	frame.AssistantIndicator.PostUpdate = UF.RaidRoleUpdate
+	frame.MasterLooterIndicator.PostUpdate = UF.RaidRoleUpdate
 
 	return anchor
 end
@@ -27,6 +30,7 @@ function UF:Configure_RaidRoleIcons(frame)
 		if not frame:IsElementEnabled('LeaderIndicator') then
 			frame:EnableElement('LeaderIndicator')
 			frame:EnableElement('AssistantIndicator')
+			frame:EnableElement('MasterLooterIndicator')
 		end
 
 		raidRoleFrameAnchor:ClearAllPoints()
@@ -39,6 +43,7 @@ function UF:Configure_RaidRoleIcons(frame)
 		raidRoleFrameAnchor:Hide()
 		frame:DisableElement('LeaderIndicator')
 		frame:DisableElement('AssistantIndicator')
+		frame:DisableElement('MasterLooterIndicator')
 	end
 end
 
@@ -47,25 +52,36 @@ function UF:RaidRoleUpdate()
 	local frame = anchor:GetParent():GetParent()
 	local leader = frame.LeaderIndicator
 	local assistant = frame.AssistantIndicator
+	local masterlooter = frame.MasterLooterIndicator
 
-	if not leader or not assistant then return; end
+	if not leader or not assistant or not masterlooter then return; end
 
 	local db = frame.db
 	local isLeader = leader:IsShown()
 	local isAssist = assistant:IsShown()
+	local isMasterLooter = masterlooter:IsShown()
 
 	leader:ClearAllPoints()
 	assistant:ClearAllPoints()
+	masterlooter:ClearAllPoints()
 
 	if db and db.raidRoleIcons then
 		if isLeader and db.raidRoleIcons.position == 'TOPLEFT' then
 			leader:Point('LEFT', anchor, 'LEFT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
+			masterlooter:Point('LEFT', leader, 'RIGHT')
 		elseif isLeader and db.raidRoleIcons.position == 'TOPRIGHT' then
 			leader:Point('RIGHT', anchor, 'RIGHT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
+			masterlooter:Point('RIGHT', leader, 'LEFT')
 		elseif isAssist and db.raidRoleIcons.position == 'TOPLEFT' then
 			assistant:Point('LEFT', anchor, 'LEFT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
+			masterlooter:Point('LEFT', assistant, 'RIGHT')
 		elseif isAssist and db.raidRoleIcons.position == 'TOPRIGHT' then
 			assistant:Point('RIGHT', anchor, 'RIGHT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
+			masterlooter:Point('RIGHT', assistant, 'LEFT')
+		elseif isMasterLooter and db.raidRoleIcons.position == 'TOPLEFT' then
+			masterlooter:Point('LEFT', anchor, 'LEFT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
+		elseif isMasterLooter and db.raidRoleIcons.position == 'TOPRIGHT' then
+			masterlooter:Point('RIGHT', anchor, 'RIGHT', db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset)
 		end
 	end
 end
