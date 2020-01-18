f7f4f305120d48930dd9c1bbd40ab723fb34567d
Azilroka <darktemplar9900@yahoo.com>
2020-01-18 16:59:45 -0600

    Gold DT - make sure these are set in the right order.

--- ElvUI/Modules/DataTexts/Gold.lua
+++ ElvUI/Modules/DataTexts/Gold.lua
@@ -13,36 +13,36 @@ local GetMoney = GetMoney
 local IsControlKeyDown = IsControlKeyDown
 local IsLoggedIn = IsLoggedIn
 local IsShiftKeyDown = IsShiftKeyDown
-local C_WowTokenPublic = C_WowTokenPublic
-local C_Timer_NewTicker = C_Timer.NewTicker
 local PRIEST_COLOR = RAID_CLASS_COLORS.PRIEST
 -- GLOBALS: ElvDB
 
-local Ticker
 local Profit, Spent = 0, 0
-local resetCountersFormatter = strjoin("", "|cffaaaaaa", L["Reset Counters: Hold Shift + Left Click"], "|r")
-local resetInfoFormatter = strjoin("", "|cffaaaaaa", L["Reset Data: Hold Shift + Right Click"], "|r")
+local resetCountersFormatter = strjoin('', '|cffaaaaaa', L["Reset Counters: Hold Shift + Right Click"], '|r')
+local resetInfoFormatter = strjoin('', '|cffaaaaaa', L["Reset Data: Hold Shift + Right Click"], '|r')
 
 local function OnEvent(self)
 	if not IsLoggedIn() then return end
 
-	if not Ticker then
-		C_WowTokenPublic.UpdateMarketPrice()
-		Ticker = C_Timer_NewTicker(60, C_WowTokenPublic.UpdateMarketPrice)
-	end
+	ElvDB = ElvDB or {}
 
-	local NewMoney = GetMoney()
-	ElvDB = ElvDB or { }
 	ElvDB.gold = ElvDB.gold or {}
 	ElvDB.gold[E.myrealm] = ElvDB.gold[E.myrealm] or {}
-	ElvDB.gold[E.myrealm][E.myname] = ElvDB.gold[E.myrealm][E.myname] or NewMoney
 
 	ElvDB.class = ElvDB.class or {}
 	ElvDB.class[E.myrealm] = ElvDB.class[E.myrealm] or {}
 	ElvDB.class[E.myrealm][E.myname] = E.myclass
 
-	local OldMoney = ElvDB.gold[E.myrealm][E.myname] or NewMoney
+	--prevent an error possibly from really old profiles
+	local oldMoney = ElvDB.gold[E.myrealm][E.myname]
+	if oldMoney and type(oldMoney) ~= 'number' then
+		ElvDB.gold[E.myrealm][E.myname] = nil
+		oldMoney = nil
+	end
 
+	local NewMoney = GetMoney()
+	ElvDB.gold[E.myrealm][E.myname] = NewMoney
+
+	local OldMoney = oldMoney or NewMoney
 	local Change = NewMoney-OldMoney -- Positive if we gain money
 	if OldMoney>NewMoney then		-- Lost Money
 		Spent = Spent - Change
@@ -50,15 +50,13 @@ local function OnEvent(self)
 		Profit = Profit + Change
 	end
 
-	self.text:SetText(E:FormatMoney(NewMoney, E.db.datatexts.goldFormat or "BLIZZARD", not E.db.datatexts.goldCoins))
-
-	ElvDB.gold[E.myrealm][E.myname] = NewMoney
+	self.text:SetText(E:FormatMoney(NewMoney, E.db.datatexts.goldFormat or 'BLIZZARD', not E.db.datatexts.goldCoins))
 end
 
 local function Click(self, btn)
-	if btn == "RightButton" then
+	if btn == 'RightButton' then
 		if IsShiftKeyDown() then
-			ElvDB.gold = nil
+			wipe(ElvDB.gold)
 			OnEvent(self)
 			DT.tooltip:Hide()
 		elseif IsControlKeyDown() then
@@ -75,7 +73,7 @@ local myGold = {}
 local function OnEnter(self)
 	DT:SetupTooltip(self)
 	local textOnly = not E.db.datatexts.goldCoins and true or false
-	local style = E.db.datatexts.goldFormat or "BLIZZARD"
+	local style = E.db.datatexts.goldFormat or 'BLIZZARD'
 
 	DT.tooltip:AddLine(L["Session:"])
 	DT.tooltip:AddDoubleLine(L["Earned:"], E:FormatMoney(Profit, style, textOnly), 1, 1, 1, 1, 1, 1)
@@ -93,13 +91,13 @@ local function OnEnter(self)
 	wipe(myGold)
 	for k,_ in pairs(ElvDB.gold[E.myrealm]) do
 		if ElvDB.gold[E.myrealm][k] then
-			local class = ElvDB.class[E.myrealm][k] or "PRIEST"
+			local class = ElvDB.class[E.myrealm][k] or 'PRIEST'
 			local color = E:ClassColor(class) or PRIEST_COLOR
 			tinsert(myGold,
 				{
 					name = k,
 					amount = ElvDB.gold[E.myrealm][k],
-					amountText = E:FormatMoney(ElvDB.gold[E.myrealm][k], E.db.datatexts.goldFormat or "BLIZZARD", not E.db.datatexts.goldCoins),
+					amountText = E:FormatMoney(ElvDB.gold[E.myrealm][k], E.db.datatexts.goldFormat or 'BLIZZARD', not E.db.datatexts.goldCoins),
 					r = color.r, g = color.g, b = color.b,
 				}
 			)
@@ -108,7 +106,7 @@ local function OnEnter(self)
 	end
 
 	for _, g in ipairs(myGold) do
-		DT.tooltip:AddDoubleLine(g.name == E.myname and g.name.." |TInterface\\COMMON\\Indicator-Green:14|t" or g.name, g.amountText, g.r, g.g, g.b, 1, 1, 1)
+		DT.tooltip:AddDoubleLine(g.name == E.myname and g.name..' |TInterface/COMMON/Indicator-Green:14|t' or g.name, g.amountText, g.r, g.g, g.b, 1, 1, 1)
 	end
 
 	DT.tooltip:AddLine(' ')
