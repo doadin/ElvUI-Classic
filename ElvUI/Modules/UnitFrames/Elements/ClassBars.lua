44f4a6cb33d3563992e384463d15998372e8cde9
Simpy <kodi03466@hotmail.com>
2020-02-24 16:05:54 -0500

    dont need that anymore

--- ElvUI/Modules/UnitFrames/Elements/ClassBars.lua
+++ ElvUI/Modules/UnitFrames/Elements/ClassBars.lua
@@ -271,7 +271,7 @@ local function ToggleResourceBar(bars, overrideVisibility)
 
 	if not frame.CLASSBAR_DETACHED then --Only update when necessary
 		UF:Configure_HealthBar(frame)
-		UF:Configure_Portrait(frame, true) --running :Hide on portrait makes the frame all funky
+		UF:Configure_Portrait(frame)
 	end
 end
 UF.ToggleResourceBar = ToggleResourceBar --Make available to combobar
