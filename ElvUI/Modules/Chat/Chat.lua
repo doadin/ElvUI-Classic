44cc86109e811e5ea41f688ccb31ba0096c6ab4e
Simpy <kodi03466@hotmail.com>
2020-05-26 03:31:37 -0400

    cleared wrong table :) lol

--- ElvUI/Modules/Chat/Chat.lua
+++ ElvUI/Modules/Chat/Chat.lua
@@ -2639,7 +2639,7 @@ function CH:GetPlayerInfoByGUID(guid)
 end
 
 function CH:ResetHistory()
-	ElvCharacterDB.ChatEditHistory = {}
+	ElvCharacterDB.ChatHistoryLog = {}
 end
 
 function CH:Initialize()
