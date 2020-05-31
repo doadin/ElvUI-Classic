3cc02fcec316674d4dc7c7f6118ed0adf9da4326
Simpy <kodi03466@hotmail.com>
2020-05-31 18:33:55 -0400

    more mover stuff - needs testing

--- ElvUI/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Modules/Tooltip/Tooltip.lua
@@ -741,9 +741,9 @@ end
 function TT:Initialize()
 	self.db = E.db.tooltip
 
-	_G.BNToastFrame:Size(_G.BNToastFrame:GetSize()) -- might need this for the mover
 	_G.BNToastFrame:Point('TOPRIGHT', _G.MMHolder, 'BOTTOMRIGHT', 0, -10)
 	E:CreateMover(_G.BNToastFrame, 'BNETMover', L["BNet Frame"], nil, nil, PostBNToastMove)
+	_G.BNToastFrame.mover:SetSize(_G.BNToastFrame:GetSize())
 	self:SecureHook(_G.BNToastFrame, "SetPoint", "RepositionBNET")
 
 	if E.private.tooltip.enable ~= true then return end
