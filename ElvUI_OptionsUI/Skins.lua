586fe1b0772e5a5775f621b477935371668d78ed
Azilroka <darktemplar9900@yahoo.com>
2020-06-19 22:18:18 -0500

    Options: Skins

--- ElvUI_OptionsUI/Skins.lua
+++ ElvUI_OptionsUI/Skins.lua
@@ -3,90 +3,59 @@ local C, L = unpack(select(2, ...))
 local B = E:GetModule('Blizzard')
 local ACH = E.Libs.ACH
 
-E.Options.args.skins = {
-	type = 'group',
-	name = L["Skins"],
-	childGroups = "tabs",
-	order = 2,
-	args = {
-		intro = ACH:Description(L["SKINS_DESC"], 0),
-		general = {
-			order = 1,
-			type = 'multiselect',
-			name = L["General"],
-			get = function(info, key)
-				if key == 'blizzardEnable' then
-					return E.private.skins.blizzard.enable
-				else
-					return E.private.skins[key]
-				end
-			end,
-			set = function(info, key, value)
-				if key == 'blizzardEnable' then
-					E.private.skins.blizzard.enable = value
-				else
-					E.private.skins[key] = value
-				end
-				E:StaticPopup_Show("PRIVATE_RL")
-			end,
-			sortByValue = true,
-			values = {
-				ace3Enable = "Ace3",
-				blizzardEnable = L["Blizzard"],
-				checkBoxSkin = L["CheckBox Skin"],
-				parchmentRemoverEnable = L["Parchment Remover"],
-			},
-		},
-		blizzard = {
-			order = 3,
-			type = 'multiselect',
-			name = L["Blizzard"],
-			get = function(info, key) return E.private.skins.blizzard[key] end,
-			set = function(info, key, value) E.private.skins.blizzard[key] = value; E:StaticPopup_Show("PRIVATE_RL") end,
-			disabled = function() return not E.private.skins.blizzard.enable end,
-			values = {
-				auctionhouse = L["Auctions"],
-				addonManager = L["AddOn Manager"],
-				bags = L["Bags"],
-				battlefield = L["Battlefield Frame"],
-				bgscore = L["BG Score"],
-				binding = L["Key Binding"],
-				BlizzardOptions = L["Interface Options"],
-				Channels  = L["CHANNELS"],
-				character = L["Character Frame"],
-				craft = L["Craft"],
-				debug = L["Debug Tools"],
-				dressingroom = L["Dressing Room"],
-				friends = L["Friends"],
-				GMChat = L["GM Chat"],
-				gossip = L["Gossip Frame"],
-				guildregistrar = L["Guild Registrar"],
-				help = L["Help Frame"],
-				inspect = L["Inspect"],
-				loot = L["Loot Frames"],
-				lootRoll = L["Loot Roll"],
-				macro = L["Macros"],
-				mail = L["Mail"],
-				merchant = L["Merchant"],
-				misc = L["Misc Frames"],
-				petition = L["Petition Frame"],
-				quest = L["Quest Frames"],
-				questtimers = L["QuestTimers Frame"],
-				raid = L["Raid Frame"],
-				spellbook = L["Spellbook"],
-				stable = L["Stable"],
-				tabard = L["Tabard Frame"],
-				talent = L["Talents"],
-				taxi = L["Taxi Frame"],
-				timemanager = L["TIMEMANAGER_TITLE"],
-				tooltip = L["Tooltip"],
-				trade = L["Trade"],
-				tradeskill = L["Tradeskills"],
-				trainer = L["Trainer Frame"],
-				tutorial = L["Tutorial Frame"],
-				worldmap = L["World Map"],
-				mirrorTimers = L["Mirror Timers"],
-			}
-		}
-	}
-};
+E.Options.args.skins = ACH:Group(L["Skins"], nil, 2, 'tab')
+E.Options.args.skins.args.intro = ACH:Description(L["SKINS_DESC"], 0)
+E.Options.args.skins.args.general = ACH:MultiSelect(L["General"], nil, 1, nil, nil, nil, function(_, key) if key == 'blizzardEnable' then return E.private.skins.blizzard.enable else return E.private.skins[key] end end, function(_, key, value) if key == 'blizzardEnable' then E.private.skins.blizzard.enable = value else E.private.skins[key] = value end E:StaticPopup_Show("PRIVATE_RL") end)
+E.Options.args.skins.args.general.sortByValue = true
+E.Options.args.skins.args.general.values = {
+	ace3Enable = "Ace3",
+	blizzardEnable = L["Blizzard"],
+	checkBoxSkin = L["CheckBox Skin"],
+	cleanBossButton = L["Clean Boss Button"],
+	parchmentRemoverEnable = L["Parchment Remover"],
+}
+
+E.Options.args.skins.args.blizzard = ACH:MultiSelect(L["Blizzard"], nil, 3, nil, nil, nil, function(_, key) return E.private.skins.blizzard[key] end, function(_, key, value) E.private.skins.blizzard[key] = value; E:StaticPopup_Show("PRIVATE_RL") end, function() return not E.private.skins.blizzard.enable end)
+E.Options.args.skins.args.blizzard.values = {
+	auctionhouse = L["Auctions"],
+	addonManager = L["AddOn Manager"],
+	bags = L["Bags"],
+	battlefield = L["Battlefield Frame"],
+	bgscore = L["BG Score"],
+	binding = L["Key Binding"],
+	BlizzardOptions = L["Interface Options"],
+	Channels  = L["CHANNELS"],
+	character = L["Character Frame"],
+	craft = L["Craft"],
+	debug = L["Debug Tools"],
+	dressingroom = L["Dressing Room"],
+	friends = L["Friends"],
+	GMChat = L["GM Chat"],
+	gossip = L["Gossip Frame"],
+	guildregistrar = L["Guild Registrar"],
+	help = L["Help Frame"],
+	inspect = L["Inspect"],
+	loot = L["Loot Frames"],
+	lootRoll = L["Loot Roll"],
+	macro = L["Macros"],
+	mail = L["Mail"],
+	merchant = L["Merchant"],
+	misc = L["Misc Frames"],
+	petition = L["Petition Frame"],
+	quest = L["Quest Frames"],
+	questtimers = L["QuestTimers Frame"],
+	raid = L["Raid Frame"],
+	spellbook = L["Spellbook"],
+	stable = L["Stable"],
+	tabard = L["Tabard Frame"],
+	talent = L["Talents"],
+	taxi = L["Taxi Frame"],
+	timemanager = L["TIMEMANAGER_TITLE"],
+	tooltip = L["Tooltip"],
+	trade = L["Trade"],
+	tradeskill = L["Tradeskills"],
+	trainer = L["Trainer Frame"],
+	tutorial = L["Tutorial Frame"],
+	worldmap = L["World Map"],
+	mirrorTimers = L["Mirror Timers"],
+}
