0c7bef78e34635e0ef08211392fb642f906106f4
Simpy <kodi03466@hotmail.com>
2020-07-09 01:30:49 -0400

    go away

--- ElvUI/Modules/Tooltip/Tooltip.lua
+++ ElvUI/Modules/Tooltip/Tooltip.lua
@@ -379,7 +379,7 @@ function TT:GameTooltip_OnTooltipSetItem(tt)
 		return
 	end
 
-	if not tt.itemCleared and TT:IsModKeyDown() then
+	if not tt.itemCleared then
 		local _, link = tt:GetItem()
 		local num = GetItemCount(link)
 		local numall = GetItemCount(link,true)
