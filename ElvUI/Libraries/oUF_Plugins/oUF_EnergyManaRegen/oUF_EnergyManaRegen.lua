21ddc71487074744c50c4ee4a84804cb9ce6b299
Merten Peetz <dev@morbz.net>
2020-07-20 14:09:08 +0200

    EMR: Sync next tick time after mp5 delay ends; References #19

--- ElvUI/Libraries/oUF_Plugins/oUF_EnergyManaRegen/oUF_EnergyManaRegen.lua
+++ ElvUI/Libraries/oUF_Plugins/oUF_EnergyManaRegen/oUF_EnergyManaRegen.lua
@@ -12,11 +12,12 @@ local UnitPowerMax = UnitPowerMax
 local GetSpellPowerCost = GetSpellPowerCost
 
 local LastTickTime = GetTime()
+local TickDelay = 2.025 -- Average tick time is slightly over 2 seconds
 local CurrentValue = UnitPower('player')
 local LastValue = CurrentValue
-local allowPowerEvent = true
-local myClass = select(2, UnitClass("player"))
+local myClass = select(2, UnitClass('player'))
 local Mp5Delay = 5
+local Mp5DelayWillEnd = nil
 local Mp5IgnoredSpells = {
 	[11689] = true, -- life tap 6
 	[11688] = true, -- life tap 5
@@ -28,82 +29,97 @@ local Mp5IgnoredSpells = {
 	[18183] = true, -- improved life tap 2
 }
 
+-- Sets tick time to the last possible time based on the last tick
+local UpdateTickTime = function(now)
+	LastTickTime = now - ((now - LastTickTime) % TickDelay)
+end
+
 local Update = function(self, elapsed)
 	local element = self.EnergyManaRegen
-
 	element.sinceLastUpdate = (element.sinceLastUpdate or 0) + (tonumber(elapsed) or 0)
 
 	if element.sinceLastUpdate > 0.01 then
-		local powerType = UnitPowerType("player")
-
+		local powerType = UnitPowerType('player')
 		if powerType ~= Enum.PowerType.Energy and powerType ~= Enum.PowerType.Mana then
 			element.Spark:Hide()
 			return
 		end
 
 		CurrentValue = UnitPower('player', powerType)
-
-		if powerType == Enum.PowerType.Mana and (not CurrentValue or CurrentValue >= UnitPowerMax('player', Enum.PowerType.Mana)) then
-			element:SetValue(0)
-			element.Spark:Hide()
-			return
-		end
-
+		local MaxPower = UnitPowerMax('player', powerType)
 		local Now = GetTime()
-		local Timer = Now - LastTickTime
 
-		if (CurrentValue > LastValue) or powerType == Enum.PowerType.Energy and (Now >= LastTickTime + 2) then
-			LastTickTime = Now
+		if powerType == Enum.PowerType.Mana then
+			if CurrentValue >= MaxPower then
+				element:SetValue(0)
+				element.Spark:Hide()
+				return
+			end
+
+			-- Sync last tick time after 5 seconds are over
+			if Mp5DelayWillEnd and Mp5DelayWillEnd < Now then
+				Mp5DelayWillEnd = nil
+				UpdateTickTime(Now)
+			end
+		elseif powerType == Enum.PowerType.Energy then
+			-- If energy is not full we just wait for the next tick
+			if Now >= LastTickTime + TickDelay and CurrentValue >= MaxPower then
+				UpdateTickTime(Now)
+			end
 		end
 
-		if Timer > 0 then
-			element.Spark:Show()
-			element:SetMinMaxValues(0, 2)
-			element.Spark:SetVertexColor(1, 1, 1, 1)
-			element:SetValue(Timer)
-			allowPowerEvent = true
-
-			LastValue = CurrentValue
-		elseif Timer < 0 then
-			-- if negative, it's mp5delay
+		if Mp5DelayWillEnd and powerType == Enum.PowerType.Mana then
+			-- Show 5 second indicator
 			element.Spark:Show()
 			element:SetMinMaxValues(0, Mp5Delay)
 			element.Spark:SetVertexColor(1, 1, 0, 1)
-
-			element:SetValue(-Timer)
+			element:SetValue(Mp5DelayWillEnd - Now)
+		else
+			-- Show tick indicator
+			element.Spark:Show()
+			element:SetMinMaxValues(0, TickDelay)
+			element.Spark:SetVertexColor(1, 1, 1, 1)
+			element:SetValue(Now - LastTickTime)
 		end
 
 		element.sinceLastUpdate = 0
 	end
 end
 
-local EventHandler = function(self, event, _, _, spellID)
-	local powerType = UnitPowerType("player")
+local OnUnitPowerUpdate = function()
+	local powerType = UnitPowerType('player')
+	if powerType ~= Enum.PowerType.Mana and powerType ~= Enum.PowerType.Energy then
+		return
+	end
 
+	-- We also register ticks from mp5 gear within the 5-second-rule to get a more accurate sync later.
+	-- Unfortunately this registers a tick when a mana pot or life tab is used.
+	local CurrentValue = UnitPower('player', powerType)
+	if CurrentValue > LastValue then
+		LastTickTime = GetTime()
+	end
+	LastValue = CurrentValue
+end
+
+local OnUnitSpellcastSucceeded = function(_, _, _, _, spellID)
+	local powerType = UnitPowerType('player')
 	if powerType ~= Enum.PowerType.Mana then
 		return
 	end
 
-	if event == 'UNIT_POWER_UPDATE' and allowPowerEvent then
-		LastTickTime = GetTime()
+	local spellCost = false
+	local costTable = GetSpellPowerCost(spellID)
+	for _, costInfo in next, costTable do
+		if costInfo.cost then
+			spellCost = true
+		end
 	end
 
-	if event == 'UNIT_SPELLCAST_SUCCEEDED' then
-		local spellCost = false
-		local costTable = GetSpellPowerCost(spellID)
-		for _, costInfo in next, costTable do
-			if costInfo.cost then
-				spellCost = true
-			end
-		end
-
-		if not spellCost or Mp5IgnoredSpells[spellID] then
-			return
-		end
-
-		LastTickTime = GetTime() + 5
-		allowPowerEvent = false
+	if not spellCost or Mp5IgnoredSpells[spellID] then
+		return
 	end
+
+	Mp5DelayWillEnd = GetTime() + 5
 end
 
 local Path = function(self, ...)
@@ -114,7 +130,7 @@ local Enable = function(self, unit)
 	local element = self.EnergyManaRegen
 	local Power = self.Power
 
-	if (unit == "player") and element and Power and myClass ~= 'WARRIOR' then
+	if (unit == 'player') and element and Power and myClass ~= 'WARRIOR' then
 		element.__owner = self
 
 		if(element:IsObjectType('StatusBar')) then
@@ -131,8 +147,8 @@ local Enable = function(self, unit)
 			spark:SetPoint('CENTER', element:GetStatusBarTexture(), 'RIGHT')
 		end
 
-		self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", EventHandler)
-		self:RegisterEvent("UNIT_POWER_UPDATE", EventHandler)
+		self:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED', OnUnitSpellcastSucceeded)
+		self:RegisterEvent('UNIT_POWER_UPDATE', OnUnitPowerUpdate)
 
 		element:SetScript('OnUpdate', function(_, elapsed) Path(self, elapsed) end)
 
@@ -145,14 +161,14 @@ local Disable = function(self)
 	local Power = self.Power
 
 	if (Power) and (element) then
-		self:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED", EventHandler)
-		self:UnregisterEvent("UNIT_POWER_UPDATE", EventHandler)
+		self:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED', OnUnitSpellcastSucceeded)
+		self:UnregisterEvent('UNIT_POWER_UPDATE', OnUnitPowerUpdate)
 
 		element.Spark:Hide()
-		element:SetScript("OnUpdate", nil)
+		element:SetScript('OnUpdate', nil)
 
 		return false
 	end
 end
 
-oUF:AddElement("EnergyManaRegen", Path, Enable, Disable)
+oUF:AddElement('EnergyManaRegen', Path, Enable, Disable)
