204309c0e1be8b1d14b5996c5ce1fd91f447a845
Azilroka <darktemplar9900@yahoo.com>
2020-08-24 19:51:42 -0500

    Select Width Adjustments

--- ElvUI_OptionsUI/UnitFrames.lua
+++ ElvUI_OptionsUI/UnitFrames.lua
@@ -1352,6 +1352,7 @@ local function CreateCustomTextGroup(unit, objectName)
 				order = 4,
 				name = L["Font"],
 				values = _G.AceGUIWidgetLSMlists.font,
+				width = 'double',
 			},
 			size = {
 				order = 5,
@@ -2716,6 +2717,7 @@ local function GetOptionsTable_GeneralGroup(updateFunc, groupName, numUnits)
 			name = L["Smart Aura Position"],
 			desc = L["Will show Buffs in the Debuff position when there are no Debuffs active, or vice versa."],
 			values = smartAuraPositionValues,
+			width = 'double',
 		}
 	end
 
@@ -2748,6 +2750,7 @@ local function GetOptionsTable_GeneralGroup(updateFunc, groupName, numUnits)
 					desc = L["Growth direction from the first unitframe."],
 					type = 'select',
 					values = growthDirectionValues,
+					width = 'double',
 				},
 				numGroups = {
 					order = 7,
@@ -2843,6 +2846,7 @@ local function GetOptionsTable_GeneralGroup(updateFunc, groupName, numUnits)
 					desc = L["Set the order that the group will sort."],
 					customWidth = 250,
 					type = 'select',
+					width = 'double',
 					values = {
 						CLASS = L["CLASS"],
 						CLASSROLE = L["CLASS"]..' & '..L["ROLE"],
