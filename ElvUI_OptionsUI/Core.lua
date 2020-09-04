f45edc608f863e0acc5201a55f121654acf6e222
Simpy <kodi03466@hotmail.com>
2020-09-04 00:23:38 -0400

    1.28

--- ElvUI_OptionsUI/Core.lua
+++ ElvUI_OptionsUI/Core.lua
@@ -457,102 +457,34 @@ local function ExportImport_Open(mode)
 end
 
 --Create Profiles Table
-E.Options.args.profiles = {
-	order = 5,
-	type = "group",
-	childGroups = "tab",
-	name = L["Profiles"],
-	args = {
-		profile = E.Libs.AceDBOptions:GetOptionsTable(E.data),
-		private = E.Libs.AceDBOptions:GetOptionsTable(E.charSettings),
-		distribute = {
-			type = "group",
-			order = 3,
-			childGroups = "tab",
-			name = L["Import / Export"],
-			args = {
-				desc = ACH:Description(L["This feature will allow you to transfer settings to other characters."], 0),
-				distributeProfile = {
-					name = L["Share Current Profile"],
-					desc = L["Sends your current profile to your target."],
-					type = "execute",
-					order = 1,
-					func = function()
-						if not UnitExists("target") or not UnitIsPlayer("target")
-						or not UnitIsFriend("player", "target") or UnitIsUnit("player", "target") then
-							E:Print(L["You must be targeting a player."])
-							return
-						end
+E.Options.args.profiles = ACH:Group(L["Profiles"], nil, 5, 'tab')
+E.Options.args.profiles.args.desc = ACH:Description(L["This feature will allow you to transfer settings to other characters."], 0)
+E.Options.args.profiles.args.distributeProfile = ACH:Execute(L["Share Current Profile"], L["Sends your current profile to your target."], 1, function() if not UnitExists('target') or not UnitIsPlayer('target') or not UnitIsFriend('player', 'target') or UnitIsUnit('player', 'target') then E:Print(L["You must be targeting a player."]) return end local name, server = UnitName('target') if name and (not server or server == '') then D:Distribute(name) elseif server then D:Distribute(name, true) end end, nil, nil, nil, nil, nil, function() return not E.global.general.allowDistributor end)
+E.Options.args.profiles.args.distributeGlobal = ACH:Execute(L["Share Filters"], L["Sends your filter settings to your target."], 1, function() if not UnitExists('target') or not UnitIsPlayer('target') or not UnitIsFriend('player', 'target') or UnitIsUnit('player', 'target') then E:Print(L["You must be targeting a player."]) return end local name, server = UnitName('target') if name and (not server or server == '') then D:Distribute(name, false, true) elseif server then D:Distribute(name, true, true) end end, nil, nil, nil, nil, nil, function() return not E.global.general.allowDistributor end)
+E.Options.args.profiles.args.exportProfile = ACH:Execute(L["Export Profile"], nil, 4, function() ExportImport_Open('export') end)
+E.Options.args.profiles.args.importProfile = ACH:Execute(L["Import Profile"], nil, 5, function() ExportImport_Open('import') end)
+E.Options.args.profiles.args.allowDistributor = ACH:Toggle(L["Allow Sharing"], L["Both users will need this option enabled."], 6, nil, nil, nil, function() return E.global.general.allowDistributor end, function(_, value) E.global.general.allowDistributor = value; D:UpdateSettings() end)
+E.Options.args.profiles.args.spacer = ACH:Spacer(6)
 
-						local name, server = UnitName("target")
-						if name and (not server or server == "") then
-							D:Distribute(name)
-						elseif server then
-							D:Distribute(name, true)
-						end
-					end
-				},
-				distributeGlobal = {
-					name = L["Share Filters"],
-					desc = L["Sends your filter settings to your target."],
-					type = "execute",
-					order = 2,
-					func = function()
-						if not UnitExists("target") or not UnitIsPlayer("target")
-						or not UnitIsFriend("player", "target") or UnitIsUnit("player", "target") then
-							E:Print(L["You must be targeting a player."])
-							return
-						end
-
-						local name, server = UnitName("target")
-						if name and (not server or server == "") then
-							D:Distribute(name, false, true)
-						elseif server then
-							D:Distribute(name, true, true)
-						end
-					end
-				},
-				spacer2 = ACH:Spacer(3),
-				exportProfile = {
-					name = L["Export Profile"],
-					type = "execute",
-					order = 4,
-					func = function()
-						ExportImport_Open("export")
-					end
-				},
-				importProfile = {
-					name = L["Import Profile"],
-					type = "execute",
-					order = 5,
-					func = function()
-						ExportImport_Open("import")
-					end
-				}
-			}
-		}
-	},
-}
+E.Options.args.profiles.args.profile = E.Libs.AceDBOptions:GetOptionsTable(E.data)
+E.Options.args.profiles.args.private = E.Libs.AceDBOptions:GetOptionsTable(E.charSettings)
 
 E.Options.args.profiles.args.profile.name = L["Profile"]
 E.Options.args.profiles.args.profile.order = 1
 E.Options.args.profiles.args.private.name = L["Private"]
 E.Options.args.profiles.args.private.order = 2
 
-E.Options.args.profiles.args.private.args.choose.confirm = function(info, value)
+E.Libs.AceConfig:RegisterOptionsTable('ElvProfiles', E.Options.args.profiles.args.profile)
+E.Libs.AceConfig:RegisterOptionsTable('ElvPrivates', E.Options.args.profiles.args.private)
+
+E.Options.args.profiles.args.private.args.choose.confirm = function(_, value)
 	return format(L["Choosing Settings %s. This will reload the UI.\n\n Are you sure?"], value)
 end
 
-E.Libs.AceConfig:RegisterOptionsTable("ElvProfiles", E.Options.args.profiles.args.profile)
-E.Options.args.profiles.args.profile.args.copyfrom.confirm = function(info, value)
-	return format(L["Copy Settings from %s. This will overwrite %s profile.\n\n Are you sure?"], value, E.data:GetCurrentProfile())
-end
-
-E.Libs.AceConfig:RegisterOptionsTable("ElvPrivates", E.Options.args.profiles.args.private)
 E.Options.args.profiles.args.private.args.copyfrom.confirm = function(info, value)
-	return format(L["Copy Settings from %s. This will overwrite %s profile.\n\n Are you sure?"], value, E.charSettings:GetCurrentProfile())
+	return format(L["Copy Settings from %s. This will overwrite %s profile.\n\n Are you sure?"], value, info.handler:GetCurrentProfile())
 end
 
-if GetAddOnEnableState(nil, "ElvUI_Config") ~= 0 then
-	E:StaticPopup_Show("ELVUI_CONFIG_FOUND")
+if GetAddOnEnableState(nil, 'ElvUI_Config') ~= 0 then
+	E:StaticPopup_Show('ELVUI_CONFIG_FOUND')
 end
